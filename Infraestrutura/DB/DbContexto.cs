a5de4fcb2eb7b27a7546daa9c9bb70efaed5a484
Joao <jpereiralopes117@gmail.com>
2025-08-06 13:58:59 -0300

    Atualizações do projeto

--- Infraestrutura/DB/DbContexto.cs
+++ Infraestrutura/DB/DbContexto.cs
@@ -1,5 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
+
 namespace MinimalApi.Infraestrutura.Db;
 
-public class DbContexto {
-    
+public class DbContexto : DbContext
+{
+    private readonly IConfiguration _configuraçãoAppSettings;
+    public DbContexto(IConfiguration configuraçãoAppSettings)
+    {
+        _configuraçãoAppSettings = configuraçãoAppSettings;
+    }
+    public DbSet<Administrador> Administradores { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+                var stringConexao = _configuraçãoAppSettings.GetConnectionString("mysql")?.ToString();
+                if (!string.IsNullOrEmpty(stringConexao))
+                {
+                    optionsBuilder.UseMySql(
+                    "string de conexão",
+                    ServerVersion.AutoDetect("string de conexao")
+                );
+            }
+        }
+    }
 }
